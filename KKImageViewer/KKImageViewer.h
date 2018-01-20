0f885a9bfb9d7ad70af65bef662383f842e02aad
Keeeeen <834be6fc.asu@gmail.com>
2018-01-20 22:14:39 +0900

    added files

---
+++ KKImageViewer/KKImageViewer.h
@@ -0,0 +1,19 @@
+//
+//  KKImageViewer.h
+//  KKImageViewer
+//
+//  Created by K.Kawakami on 2018/01/20.
+//  Copyright © 2018年 Kentaro Kawakami. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KKImageViewer.
+FOUNDATION_EXPORT double KKImageViewerVersionNumber;
+
+//! Project version string for KKImageViewer.
+FOUNDATION_EXPORT const unsigned char KKImageViewerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KKImageViewer/PublicHeader.h>
+
+
