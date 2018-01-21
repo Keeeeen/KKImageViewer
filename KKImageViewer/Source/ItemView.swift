d26a0d4ecda335052e4816a1f6ddcfe2a4b26718
Keeeeen <834be6fc.asu@gmail.com>
2018-01-21 21:39:20 +0900

    added ItemView

---
+++ KKImageViewer/Source/ItemView.swift
@@ -0,0 +1,13 @@
+//
+//  ItemView.swift
+//  KKImageViewer
+//
+//  Created by K.Kawakami on 2018/01/21.
+//  Copyright © 2018年 Kentaro Kawakami. All rights reserved.
+//
+
+import Foundation
+
+public protocol ItemView {
+    var image: UIImage? { get set }
+}
