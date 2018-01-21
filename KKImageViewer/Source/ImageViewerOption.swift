723bfe6f619b0a4b317a81936c9fff0092c8d08d
Keeeeen <834be6fc.asu@gmail.com>
2018-01-21 17:21:59 +0900

    added ImageViewerOption

---
+++ KKImageViewer/Source/ImageViewerOption.swift
@@ -0,0 +1,9 @@
+//
+//  ImageViewerOption.swift
+//  KKImageViewer
+//
+//  Created by K.Kawakami on 2018/01/21.
+//  Copyright © 2018年 Kentaro Kawakami. All rights reserved.
+//
+
+import Foundation
