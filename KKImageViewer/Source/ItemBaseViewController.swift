4b60c3e6e9812e722c8cd7fe7f2710dc453683f7
Keeeeen <834be6fc.asu@gmail.com>
2018-01-21 21:57:42 +0900

    added ItemBaseViewController

---
+++ KKImageViewer/Source/ItemBaseViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ItemBaseViewController.swift
+//  KKImageViewer
+//
+//  Created by K.Kawakami on 2018/01/21.
+//  Copyright © 2018年 Kentaro Kawakami. All rights reserved.
+//
+
+import UIKit
+
+class ItemBaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
