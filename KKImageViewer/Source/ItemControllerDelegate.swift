4f4cbb855c3f1c3165ee2d16f55b42cace2d74e5
Keeeeen <834be6fc.asu@gmail.com>
2018-01-21 21:49:44 +0900

    added ItemControllerDelegate

---
+++ KKImageViewer/Source/ItemControllerDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  ItemControllerDelegate.swift
+//  KKImageViewer
+//
+//  Created by K.Kawakami on 2018/01/21.
+//  Copyright © 2018年 Kentaro Kawakami. All rights reserved.
+//
+
+import Foundation
+
+public protocol ItemControllerDelegate: class {
+    
+    ///Represents a generic transitioning progress from 0 to 1 (or reversed) where 0 is no progress and 1 is fully finished transitioning. It's up to the implementing controller to make decisions about how this value is being calculated, based on the nature of transition.
+    func itemController(_ controller: ItemController, didSwipeToDismissWithRatio ratio: CGFloat)
+    
+    func itemControllerDidFinishSwipeToDismiss()
+    
+    func itemControllerDidSingleTap(_ controller: ItemController)
+    func itemControllerDidLongPress(_ controller: ItemController, in item: ItemView)
+    
+    func itemControllerWillAppear(_ controller: ItemController)
+    func itemControllerWillDisappear(_ controller: ItemController)
+    func itemControllerDidAppear(_ controller: ItemController)
+}
