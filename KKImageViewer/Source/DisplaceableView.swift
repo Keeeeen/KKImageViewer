05c2eebcf078d5b6067675b4ab1075217944bf2f
Keeeeen <834be6fc.asu@gmail.com>
2018-01-21 19:28:51 +0900

    updates protocol DisplaceableView

--- KKImageViewer/Source/DisplaceableView.swift
+++ KKImageViewer/Source/DisplaceableView.swift
@@ -7,3 +7,33 @@
 //
 
 import Foundation
+import UIKit
+
+public protocol DisplaceableView {
+    
+    var image: UIImage? { get }
+    var bounds: CGRect { get }
+    var center: CGPoint { get }
+    var boundsCenter: CGPoint { get }
+    var contentMode: UIViewContentMode { get }
+    var isHidden: Bool { get set }
+    
+    func convert(_ point: CGPoint, to view: UIView?) -> CGPoint
+}
+
+extension DisplaceableView {
+    
+    var imageView: UIImageView {
+        
+        let imageView = UIImageView(image: image)
+        imageView.bounds = bounds
+        imageView.center = center
+        imageView.contentMode = contentMode
+        
+        return imageView
+    }
+    
+    var frameInCoordinatesOfScreen: CGRect {
+        return UIView().convert(bounds, to: UIScreen.main.coordinateSpace)
+    }
+}
