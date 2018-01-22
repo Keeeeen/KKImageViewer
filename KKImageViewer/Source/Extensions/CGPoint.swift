163d899674e33396c4018c0979de99a984c16308
Keeeeen <834be6fc.asu@gmail.com>
2018-01-22 19:24:41 +0900

    updates CGPoint extension. added direction and orientation property

--- KKImageViewer/Source/Extensions/CGPoint.swift
+++ KKImageViewer/Source/Extensions/CGPoint.swift
@@ -8,11 +8,48 @@
 
 import Foundation
 
+enum Direction {
+    
+    case left, right, up, down, none
+}
+
+enum Orientation {
+    
+    case vertical, horizontal, none
+}
 
 extension CGPoint {
     
     func inverted() -> CGPoint {
         
         return CGPoint(x: y, y: x)
+    }
+    
+    var direction: Direction {
+        
+        if x == 0 && y == 0 { return .none }
+        
+        if (abs(self.x) > abs(self.y) && self.x > 0) {
+            return .right
+        } else if (abs(self.x) > abs(self.y) && self.x <= 0) {
+            return .left
+        } else if (abs(self.x) <= abs(self.y) && self.y > 0) {
+            return .up
+        } else if (abs(self.x) <= abs(self.y) && self.y <= 0) {
+            return .down
+        } else {
+            return .none
+        }
+    }
+    
+    var orientation: Orientation {
+        
+        if direction == .none {
+            return .none
+        } else if direction == .left || direction == .right {
+            return .horizontal
+        } else {
+            return .vertical
+        }
     }
 }
