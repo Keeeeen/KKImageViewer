f8fba09f33bf5b78e173433d9f91a9504aefba1e
Keeeeen <834be6fc.asu@gmail.com>
2018-01-23 00:40:24 +0900

    added ImageViewController

---
+++ KKImageViewer/Source/ImageViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ImageViewController.swift
+//  KKImageViewer
+//
+//  Created by K.Kawakami on 2018/01/23.
+//  Copyright © 2018年 Kentaro Kawakami. All rights reserved.
+//
+
+import Foundation
+
+extension UIImageView: ItemView { }
+
+class ImageViewController: ItemBaseViewController<UIImageView> {
+    
+}
