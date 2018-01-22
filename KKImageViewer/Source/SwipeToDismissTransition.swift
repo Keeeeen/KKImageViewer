397bfd81f2527d261321458be440db2a5d9645dc
Keeeeen <834be6fc.asu@gmail.com>
2018-01-22 19:31:46 +0900

    added SwipeToDismissTransition

---
+++ KKImageViewer/Source/SwipeToDismissTransition.swift
@@ -0,0 +1,9 @@
+//
+//  SwipeToDismissTransition.swift
+//  KKImageViewer
+//
+//  Created by K.Kawakami on 2018/01/22.
+//  Copyright © 2018年 Kentaro Kawakami. All rights reserved.
+//
+
+import Foundation
