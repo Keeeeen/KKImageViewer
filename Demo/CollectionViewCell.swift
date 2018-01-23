fa3b3a95c068b49975a97eac3019d8de845a89cd
Keeeeen <834be6fc.asu@gmail.com>
2018-01-23 22:51:38 +0900

    added CollectionViewCell

---
+++ Demo/CollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CollectionViewCell.swift
+//  Demo
+//
+//  Created by K.Kawakami on 2018/01/23.
+//  Copyright © 2018年 Kentaro Kawakami. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
