3227b58c1e165255bd0a2f7149c68b74a9538220
Keeeeen <834be6fc.asu@gmail.com>
2018-01-23 15:35:04 +0900

    added ImageViewerController

---
+++ KKImageViewer/Source/ImageViewerController.swift
@@ -0,0 +1,35 @@
+//
+//  ImageViewerController.swift
+//  KKImageViewer
+//
+//  Created by K.Kawakami on 2018/01/23.
+//  Copyright © 2018年 Kentaro Kawakami. All rights reserved.
+//
+
+import UIKit
+
+open class ImageViewerController: UIPageViewController {
+
+    override open func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override open func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
