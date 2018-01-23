d1459aace0e0a268c8f045edddfee9a12cc29906
Keeeeen <834be6fc.asu@gmail.com>
2018-01-24 00:56:29 +0900

    updates ViewControlelr and Main Stroyboard

--- Demo/ViewController.swift
+++ Demo/ViewController.swift
@@ -7,19 +7,82 @@
 //
 
 import UIKit
+import KKImageViewer
+
+extension UIImageView: DisplaceableView { }
 
 class ViewController: UIViewController {
-
+    
+    private let items = [#imageLiteral(resourceName: "cat_1"), #imageLiteral(resourceName: "cat_2"), #imageLiteral(resourceName: "cat_3"), #imageLiteral(resourceName: "cat_4")]
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let nib = UINib(nibName: "CollectionViewCell", bundle: nil)
+        collectionView.register(nib, forCellWithReuseIdentifier: "cell")
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
+extension ViewController: UICollectionViewDataSource {
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as? CollectionViewCell else {
+            fatalError()
+        }
+        
+        cell.image.image = items[indexPath.row]
+        
+        return cell
+    }
+}
+
+extension ViewController: UICollectionViewDelegate {
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+        let imageViewer = ImageViewerController(
+            startIndex: indexPath.row,
+            itemDataSource: self,
+            displacedViewsDataSource: self,
+            option: ImageViewerOption()
+        )
+        
+        present(imageViewer, animated: false, completion: nil)
+    }
+}
+
+extension ViewController: ImageViewerDataSource {
+    func numberOfItems() -> Int {
+        return items.count
+    }
+    
+    func providedImageViewerItem(at index: Int) -> ImageViewerItem {
+        return ImageViewerItem.image { block in
+            block(self.items[index])
+        }
+    }
+}
+
+extension ViewController: DisplacedViewsDataSource {
+    func provideDisplacementItem(at index: Int) -> DisplaceableView? {
+        guard let cell = collectionView.cellForItem(at: IndexPath(row: index, section: 0)) as? CollectionViewCell else {
+            return nil
+        }
+        return cell.image
+    }
+}
